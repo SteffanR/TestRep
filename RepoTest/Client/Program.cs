3bb11c44cb10d02864bc8b10084053de7abf5148
Liviu Şoşu <sosuliviu@gmail.com>
2017-11-26 18:55:23 +0200

    Liviu commit to Stefan brench

--- RepoTest/Client/Program.cs
+++ RepoTest/Client/Program.cs
@@ -10,8 +10,7 @@ namespace Client
     {
         static void Main(string[] args)
         {
-//lll
-//for LIVIUS
+            //liviu added
             Console.WriteLine("Hey this is a test. Press any key to continue ...");
             Console.ReadKey();
         }
