8471ba9d11f7d52774f1270c4509df01d61ca6df
Liviu Şoşu <sosuliviu@gmail.com>
2017-11-26 18:20:26 +0200

    steffan commit

---
+++ RepoTest/Steffan2/Steffan.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Steffan2
+{
+    public static class Steffan
+    {
+        public static string SteffanMethod()
+        {
+            return "Steffan";
+        }
+    }
+}
