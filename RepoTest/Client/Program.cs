a36288498a0d08a4cd7dcf7cbdf0c05691f6b31e
Liviu Şoşu <sosuliviu@gmail.com>
2017-11-26 18:46:23 +0200

    5

--- RepoTest/Client/Program.cs
+++ RepoTest/Client/Program.cs
@@ -10,6 +10,7 @@ namespace Client
     {
         static void Main(string[] args)
         {
+            //added by Steffan
             Console.WriteLine("Hey this is a test. Press any key to continue ...");
             Console.ReadKey();
         }
